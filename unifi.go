d758c20460a43ca9015f69df1f27d48c02291be6
Patrick Wagstrom <160672+pridkett@users.noreply.github.com>
2024-09-10 10:24:38 -0400

    fix(logging): remove superfluous http logging
    There was some extra logging of HTTP parameters in the code. This isn't
    needed anymore so I just commented it out.
    
    DCO-1.1 Signed-off-by: Patrick Wagstrom <160672+pridkett@users.noreply.github.com>

--- unifi.go
+++ unifi.go
@@ -298,6 +298,9 @@ func UnifiUpdateFirewallGroup(url string, firewallGroup UnifiFirewallGroup) (Uni
 	req.Header.Set("X-CSRF-Token", CSRFToken)
 	req.Header.Set("Cookie", "TOKEN="+CookieToken)
 
+	// log.Infof("Request: %s %s", req.Method, req.URL.String())
+	// log.Infof("Headers: %v", req.Header)
+	// log.Infof("Payload: %s", string(body))
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
